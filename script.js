2d52beb1949d925fd68c448cf20ee787b6d6eae6
lokesh kallepalli <142959946+lok25esh@users.noreply.github.com>
2023-12-24 22:28:58 -0600

    script file were added

--- script.js
+++ script.js
@@ -1,13 +1,48 @@
 const accessKey="XKb-f1TCSjKPd8IRo50eHfYVPjRp-9iviOnsvrfUz7E";
 const formE1=document.querySelector('form');
 const inputE=document.getElementById("search-input");
-const searchREsults= document.querySelector(".search-results");
+const searchResults= document.querySelector(".search-results");
 const showMore=document.getElementById("btn");
 
 let inputData="";
 let page=1;
 
-function searchImages(){
+async function searchImages(){
     inputData=inputE.value;
+    const url=`https://api.unsplash.com/search/photos/?page=${page}&query=${inputData}&client_id=${accessKey}`
+    const response= await fetch(url);
+    const data=await response.json();
+    const results=data.results;
+   if(page===1){
+    searchResults.innerHTML="";
+   }
+   results.map((result)=>{
+    const imageWrapper=document.createElement("div");
+    imageWrapper.classList.add("search-result");
+    const image=document.createElement("img");
+    image.src=result.urls.small;
+    image.alt=result.alt_description;
+    const imageLink=document.createElement("a");
+    imageLink.href=result.links.html;
+    imageLink.target="_blank";
+    imageLink.textContent=result.alt_description;
+    imageWrapper.appendChild(image);
+    imageWrapper.appendChild(imageLink);
+    searchResults.appendChild(imageWrapper);
+   });
+page++;
+if(page>1){
+    showMore.style.display="block";
+
+}
 }
 
+formE1.addEventListener("submit",(event)=>{
+    event.preventDefault()
+    page=1;
+    searchImages();
+})
+showMore.addEventListener("click",()=>{
+    
+    searchImages();
+})
